4c8ed37dd414bf6e8a9dbb2e09bf09f4bf6d4878
DucHai2907Tran <haitranduc01@gmail.com>
2020-06-19 16:11:09 +0700

    MVC buổi 2

--- Unit5_MVC/controllers/CategoryController.php
+++ Unit5_MVC/controllers/CategoryController.php
@@ -35,5 +35,33 @@
                 header('Location: ?mod=category&act=add');
             }
         }
+        public function edit(){
+        	$id=isset($_GET['id'])?$_GET['id']:0;
+
+        	$cate = $this->cate_model->find($id);
+
+            require_once('views/category/edit.php');
+        }
+        public function update(){
+            $data = array();
+            $data['id'] = $_POST['id'];
+            $data['title'] = $_POST['title'];
+            $data['description'] = $_POST['description'];
+            $status = $this->cate_model->update($data);
+            if ($status == true) {
+                header('Location: ?mod=category');
+            } else {
+                header('Location: ?mod=category&act=edit&id='.$data['id']);
+            }
+        }
+        public function delete(){
+            $id=isset($_GET['id'])?$_GET['id']:0;
+
+            $cate = $this->cate_model->delete($id);
+
+            if ($cate == true) {
+                header('Location: ?mod=category');
+            }
+        }
 	}
  ?>
--- Unit5_MVC/index.php
+++ Unit5_MVC/index.php
@@ -18,7 +18,16 @@
 					break;
 				case 'store':
 					$controller_obj->store();
-					break;			
+					break;
+				case 'edit':
+					$controller_obj->edit();
+					break;	
+				case 'update':
+					$controller_obj->update();
+					break;
+				case 'delete':
+					$controller_obj->delete();
+					break;					
 				default:
 					echo "Không tồn tại action này";
 					break;
@@ -48,12 +57,21 @@
 				case 'detail':
 					$controller_obj->detail();
 					break;
-				// case 'add':
-				// 	$controller_obj->add();
-				// 	break;
-				// case 'store':
-				// 	$controller_obj->store();
-				// 	break;
+				case 'add':
+					$controller_obj->add();
+					break;
+				case 'store':
+					$controller_obj->store();
+					break;
+				case 'edit':
+					$controller_obj->edit();
+					break;	
+				case 'update':
+					$controller_obj->update();
+					break;
+				case 'delete':
+					$controller_obj->delete();
+					break;	
 				default:
 					echo "Không tồn tại action này";
 					break;
